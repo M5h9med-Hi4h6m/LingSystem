c61a3175ed750962c0ad3e35e687802d1fdf5de8
LingSystem <111419977+styele@users.noreply.github.com>
2023-04-07 13:00:15 +0200

    Create Azkar.js

---
+++ SRC/Util/Azkar.js
@@ -0,0 +1,15 @@
+const AzkarData = require("../Data/Azkar/Lang");
+
+/**
+ * @typedef {Object} Azkar
+ * @property {number} Arabic AzkarData.Ar
+ */
+
+/**
+ * @type {Azkar}
+ * @ignore
+ */
+
+module.exports = {
+  Arabic: AzkarData.Arabic,
+};
